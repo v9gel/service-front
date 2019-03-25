1c3faae7afcd200868396ac58f7d125245a10378
Vyacheslav <v9gel@mail.ru>
2019-03-25 18:58:48 +0700

    Редактируемые поля таблицы

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -1,25 +1,43 @@
 <template>
     <div>
         <h2>Клиенты</h2>
+        <el-button type="primary" @click="dialogVisible = true">Добавить клиента</el-button>
+
         <Line></Line>
         <el-table
                 :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
                 style="width: 100%">
             <el-table-column
                     label="Фамилия"
-                    prop="lastname">
+                    prop="surname">
+                <template slot-scope="scope">
+                    <el-input v-model="scope.row.surname" v-if="scope.row.editable"></el-input>
+                    <div  v-if="!scope.row.editable">{{scope.row.surname}}</div>
+                </template>
             </el-table-column>
             <el-table-column
                     label="Имя"
                     prop="name">
+                <template slot-scope="scope">
+                    <el-input v-model="scope.row.name" v-if="scope.row.editable"></el-input>
+                    <div  v-if="!scope.row.editable">{{scope.row.name}}</div>
+                </template>
             </el-table-column>
             <el-table-column
                     label="Отчество"
-                    prop="surname">
+                    prop="patronymic">
+                <template slot-scope="scope">
+                    <el-input v-model="scope.row.patronymic" v-if="scope.row.editable"></el-input>
+                    <div  v-if="!scope.row.editable">{{scope.row.patronymic}}</div>
+                </template>
             </el-table-column>
             <el-table-column
                     label="Телефон"
                     prop="phone">
+                <template slot-scope="scope">
+                    <el-input v-model="scope.row.phone" v-if="scope.row.editable"></el-input>
+                    <div  v-if="!scope.row.editable">{{scope.row.phone}}</div>
+                </template>
             </el-table-column>
             <el-table-column
                     align="right">
@@ -30,11 +48,9 @@
                             placeholder="Поиск..."/>
                 </template>
                 <template slot-scope="scope">
-                    <router-link :to="'/references/clients/' + scope.row.id">
-                        <el-button
-                                size="mini"
-                                @click="handleEdit(scope.$index, scope.row)">Изменить</el-button>
-                    </router-link>
+                    <el-button
+                            size="mini"
+                            @click="scope.row.editable = !scope.row.editable">Изменить</el-button>
                     <el-button
                             size="mini"
                             type="danger"
@@ -52,36 +68,52 @@
         components: {Line},
         data() {
             return {
-                tableData: [{
-                    id: 1,
-                    lastname: 'Иванов',
-                    name: 'Иван',
-                    surname: 'Иванович',
-                    phone: '+79991230000'
-                },{
-                    id: 2,
-                    lastname: 'Иванов',
-                    name: 'Иван',
-                    surname: 'Иванович',
-                    phone: '+79991230000'
-                },{
-                    id: 3,
-                    lastname: 'Иванов',
-                    name: 'Иван',
-                    surname: 'Иванович',
-                    phone: '+79991230000'
-                }],
+                dialogVisible: false,
+                form: {
+                    surname: '',
+                    name: '',
+                    patronymic: '',
+                    phone: ''
+                },
+                tableData: null,
                 search: '',
             }
         },
         methods: {
+            handleAdd() {
+                this.axios.post(this.$config.API +'references/clients', this.form).then((response) => {
+
+                });
+                this.dialogVisible = false;
+                this.handleUpdate()
+                this.handleUpdate()
+            },
             handleEdit(index, row) {
-                console.log(index, row);
+                this.axios.post(this.$config.API +'references/clients/'+ this.row.id).then((response) => {
+
+                });
+                this.handleUpdate();
+                this.handleUpdate();
+            },
+            handleClose() {
+                this.dialogVisible = false;
             },
             handleDelete(index, row) {
-                console.log(index, row);
-            }
+                this.axios.delete(this.$config.API +'references/clients/' + row.id).then((response) => {
+
+                });
+                this.handleUpdate();
+                this.handleUpdate();
+            },
+            handleUpdate() {
+                this.axios.get(this.$config.API +'references/clients').then((response) => {
+                    this.tableData = response.data
+                });
+            },
         },
+        created() {
+            this.handleUpdate();
+        }
     }
 </script>
 
